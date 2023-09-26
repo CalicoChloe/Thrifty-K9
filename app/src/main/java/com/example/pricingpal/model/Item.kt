594f4423a2cba658ceb7add7cdd3f23284d00cc3
Chloe Jackson <chloe.jackson@protonmail.com>
2023-09-25 20:35:28 -0600

    Converted Item class into a data class with parameters for category, name, and price. Referenced files in PR#3 for parameter order.

--- app/src/main/java/com/example/pricingpal/model/Item.kt
+++ app/src/main/java/com/example/pricingpal/model/Item.kt
@@ -1,4 +1,15 @@
 package com.example.pricingpal.model
 
-class Item {
-}
+/**
+ * Data class for an item.
+ *
+ * This class defines the item class and its properties.
+ *
+ * @property category the category of this item.
+ * @property name the name of this item.
+ * @property price the price of this item.
+ *
+ * @author Chloe Jackson
+ */
+
+data class Item(val category: String, val name: String, val price: Double)
