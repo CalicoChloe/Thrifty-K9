1e22fe275a77bbfc09cb47586a46b4a166309408
Abdoulie J NJie <106096076+AbdoulieJNJie98@users.noreply.github.com>
2023-09-28 19:04:14 -0400

    Adjusted the comments for the category class

--- app/src/main/java/com/example/pricingpal/model/Category.kt
+++ app/src/main/java/com/example/pricingpal/model/Category.kt
@@ -1,10 +1,13 @@
 package com.example.pricingpal.model
 /*
- * @Abdoulie J NJie
- * This class works as a skeleton that has a primary constructor that has a parameter
- * that initializes a string that will hold the name of a category, and an array list that
- * will hold a list of items, which will contain item elements such as
- * the item's category, name, and price.
+ * Data class for a category
+ *
+ * This class defines the category class properties.
+ *
+ * @property category the name of a category.
+ * @property item an array list of Item class objects
+ *
+ * @Abdoulie NJie
  */
 
 data class Category(val category: String, val item: ArrayList<Item>)
