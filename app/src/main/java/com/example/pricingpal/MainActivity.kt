49d19dfd53772003367582ae60d9436dabb16c98
Shianne <slesure1@ggc.edu>
2023-10-04 14:07:41 -0400

    Cleaned up the UI

--- app/src/main/java/com/example/pricingpal/MainActivity.kt
+++ app/src/main/java/com/example/pricingpal/MainActivity.kt
@@ -8,34 +8,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pricingpal.model.CSVReader
@@ -54,14 +43,18 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier
                         .fillMaxSize(),
-                        //.background(Color(0xFFC7F9CC)),
-                    //color = MaterialTheme.colorScheme.background,
+                    //This changes the color of the background of the tablet.
                     color = Color(0xFFC7F9CC)
                 ) {
+                    //This is an import of the paw Image. This will sit on top of the background color.
                     Image(
+                        //Imports image from resource folder
                         painter = painterResource(id = R.drawable.paw_background),
-                        contentDescription = null,
+                        //description of the image for accessibility
+                        contentDescription = "Pictures of paws",
+                        //crops the image
                         contentScale = ContentScale.Crop,
+                        // changes the opacity of the image
                         alpha = 0.1F
                     )
                     CategoryList(categories)
@@ -71,53 +64,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-/*
-@Composable
-fun CategoryList(categories: ArrayList<Category>) {
-    LazyColumn(modifier = Modifier.fillMaxWidth()) {
-        for (category: Category in categories) {
-            item {
-                CreateListItem(text = category.category, padding = 10)
-            }
-        }
-    }
-}
 
 @Composable
-fun CreateListItem(text: String, padding: Int, modifier: Modifier = Modifier) {
-    Surface(color = Color.LightGray, modifier = Modifier
-        .border
-            (
-            border = BorderStroke(2.dp, Color.Black),
-            shape = RectangleShape
-        )
-        .fillMaxWidth()) {
-        Text(
-            text = text,
-            modifier = modifier.padding(padding.dp),
-            textAlign = TextAlign.Center
-        )
-    }
-}
-
- */
-
-
-@Composable
+// this function will take categories cards and put them into a scrollable list
 fun CategoryList (categories: ArrayList<Category>){
     LazyColumn(
-        horizontalAlignment = Alignment.CenterHorizontally,
+        //aligns the categories within the center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         item {
+            //Imports image from resource folder
             Image(
                 painter = painterResource(id = R.drawable.picture2),
+                //description of the image for accessibility
                 contentDescription = "pricing pal logo",
                 modifier = Modifier
+                    // adds padding at the top of the image
                     .padding(top = 10.dp)
+                    // Changes the image size
                     .width(600.dp)
                     .height(150.dp),
             )
         }
+        // takes each category card and put into a list
         for(category : Category in categories) {
            item { CategoryCard(category = category)}
         }
@@ -127,34 +96,42 @@ fun CategoryList (categories: ArrayList<Category>){
 
 
 @Composable
+//puts the category name into a card view
 fun CategoryCard(category: Category){
     Card(modifier = Modifier
+        // padding around the card
         .padding(15.dp)
         .border(
+            // puts a border around the card
             border = BorderStroke(3.dp, Color(0xFF22577A)),
+            // shapes the card
             shape = RectangleShape
         ),
+        // puts a shadow under the card to make it pop out
         elevation = CardDefaults.cardElevation(12.dp),
-        //shape = RoundedCornerShape(16.dp)
     ) {
-
+        // Allows for the text to be put on top of the card
         Box(modifier = Modifier
+            // the size of the box
             .width(700.dp)
             .height(200.dp)
+            // changes the color of the box
             .background(Color(0xFF38A3A5)),
+            //allows for the content to be put into the center of the box
             contentAlignment = Alignment.Center,
             ){
             Text(
+                // takes the text from the category variable
                 text = category.category,
+                // changes the size of the font
                 fontSize = 70.sp,
+                // allows for the font to be in bold
                 fontWeight = FontWeight.Bold,
                 lineHeight = 116.sp,
+                //change the color of the text
                 color = Color.Black,
-                textAlign = TextAlign.Center,
                 modifier = Modifier
-                    //.padding(start = 150.dp)
                     .padding(50.dp)
-                    .align(alignment = Alignment.TopCenter),
                 )
         }
     }
