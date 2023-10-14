68692b04acc0880059997a4bad0a917df08dd82c
ConnorDMurdock <connordmurdock@gmail.com>
2023-10-13 21:55:17 -0400

    basic navigation setup

--- app/src/main/java/com/example/pricingpal/MainActivity.kt
+++ app/src/main/java/com/example/pricingpal/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,6 +30,13 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+import androidx.navigation.compose.rememberNavController
 import com.example.pricingpal.model.CSVReader
 import com.example.pricingpal.model.CSVParser
 import com.example.pricingpal.model.Category
@@ -37,12 +47,35 @@ class MainActivity : ComponentActivity() {
         setContent {
             val csvp = CSVParser
             val categories = csvp.PopulateData(CSVReader.readFile(this, "testdata.csv"))
-            CategoryScaffold(categories)
+            //CategoryScaffold(categories)
+            PricingPalNavHost(categories = categories)
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PricingPalNavHost(
+    categories: ArrayList<Category>,
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = "categoryList"
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable("categoryList") {
+            CategoryList(categories = categories, onNavigateToItems = {navController.navigate("itemList")})
+        }
+        composable("itemList") {
+            ItemList()
+        }
+    }
+}
+
+
+/*@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoryScaffold(categories: ArrayList<Category>) {
     Scaffold (
@@ -61,28 +94,32 @@ fun CategoryScaffold(categories: ArrayList<Category>) {
         //Background color for the content
         containerColor = Color(0xFFC7F9CC)
     )
-}
+}*/
 
 @Composable
 // this function will take categories cards and put them into a scrollable list
-fun CategoryList (categories: ArrayList<Category>, padding: PaddingValues){
+fun CategoryList (categories: ArrayList<Category>, onNavigateToItems: () -> Unit){
     LazyColumn(
         //aligns the categories within the center
-        modifier = Modifier.padding(padding).fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
         // takes each category card and put into a list
         for(category : Category in categories) {
-           item { CategoryCard(category = category)}
+           item { CategoryCard(category = category, onNavigateToItems = onNavigateToItems)}
         }
+
     }
 }
 
 @Composable
 //puts the category name into a card view
-fun CategoryCard(category: Category){
+fun CategoryCard(category: Category, onNavigateToItems: () -> Unit){
     Card(modifier = Modifier
+        .clickable(onClick = onNavigateToItems)
+
         // padding around the card
         .padding(15.dp)
         .border(
@@ -116,7 +153,14 @@ fun CategoryCard(category: Category){
                 color = Color.Black,
                 modifier = Modifier
                     .padding(50.dp)
-                )
+            )
         }
+    }
+}
+
+@Composable
+fun ItemList() {
+    LazyColumn() {
+        item { Text(text = "TEST") }
     }
 }
