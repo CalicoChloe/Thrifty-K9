6a8ba17378883b38060b877d44a228a2db6166f7
jellis2-GGC <89542413+jellis2-GGC@users.noreply.github.com>
2023-10-17 17:15:01 -0400

    Merged functional parts of UI and made changes to app bar

--- app/src/main/java/com/example/pricingpal/MainActivity.kt
+++ app/src/main/java/com/example/pricingpal/MainActivity.kt
@@ -3,18 +3,44 @@ package com.example.pricingpal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+//import androidx.compose.foundation.layout.BoxScopeInstance.align
+//import androidx.compose.foundation.layout.FlowColumnScopeInstance.align
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pricingpal.model.Category
 import com.example.pricingpal.model.Navigation
 import com.example.pricingpal.viewmodel.CategoryViewModel
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,18 +70,28 @@ fun CategoryScaffold(categories: HashMap<String, Category>) {
     Scaffold (
         //Create an app bar of medium size at the top of the scaffold
         topBar = {
-            MediumTopAppBar(
-                title = {
-                    // App bar title
-                    Text(text = "Pricing Pal")
-                }
+            LargeTopAppBar(
+                title = { Image(
+                    painter = painterResource(id = R.drawable.picture2),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(90.dp)
+                        .background(color = colorResource(id = R.color.pale_blue))
+                        .padding(start = 100.dp)
+                        .padding(end = 180.dp)
+                )
+                },
+                //color = Color.DarkGray,
+
             )
         },
+
         //padding automatically adjusts to match the app bar size
         content = { padding ->
            Navigation(categories = categories, padding = padding)
         },
         //Background color for the content
-        containerColor = Color(0xFFC7F9CC)
+        containerColor = colorResource(id = R.color.grey_blue)
+
     )
 }
--- app/src/main/java/com/example/pricingpal/view/ItemList.kt
+++ app/src/main/java/com/example/pricingpal/view/ItemList.kt
@@ -3,12 +3,13 @@ package com.example.pricingpal.view
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -37,25 +38,127 @@ fun ItemList(
         if (currentCategory != null) {
             //Everything above this line should not be touched! It's required to make sure the current category isn't null before attempting to use the value!
             //Everything below these comments is temporary code to display the item list. Please replace with the final UI design:
+            //stickyHeader { CategoryCard(categoryName = currentCategory)}
+            //ListHeader(name = currentCategory.category)
             LazyColumn(
                 modifier = Modifier
                     .padding(padding)
                     .fillMaxSize()
             ) {
                 item {
-                    Text(text = currentCategory.category, fontSize = 70.sp)
+                    //Text(text = currentCategory.category, fontSize = 70.sp)
+                    CategoryCard(categoryName = currentCategory)
                 }
                 item {
                     for (i in currentCategory.item) {
                         ItemCard(i)
-                }
+                    }
                 }
             }
         }
     }
 }
 
-//Copy and pasted from the CategoryList.kt file. Please replace with final UI design:
+/*@Composable
+fun ListHeader( name: String){
+    Column() {
+        Row(
+            modifier = Modifier
+                // wraps completely around the text
+                .wrapContentSize(Alignment.TopCenter, false)
+                //fills it to hit the edge of the device
+                .fillMaxWidth()
+                .height(80.dp)
+                .background(color = Color(0xFF758BFD), shape = RectangleShape)
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(top = 10.dp, start = 15.dp)
+                    .width(200.dp)
+                    .height(60.dp)
+                //.requiredSize(60.dp),
+
+            )
+
+            Surface(
+                shape = RectangleShape,
+                color = Color(0xFFAEB8FE),
+                //shadowElevation = 12.dp,
+                modifier = Modifier
+                    // wraps completely around the text
+                    .wrapContentSize(Alignment.TopCenter, false)
+                    //fills it to hit the edge of the device
+                    .size(width = 490.dp, height = 70.dp)
+                    .padding(top = 20.dp, bottom = 10.dp, start = 15.dp, end = 10.dp)
+                // .fillMaxWidth()
+            ) {
+                Text(
+                    text = name,
+                    textAlign = TextAlign.Start,
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .padding(top = 8.dp, start = 5.dp)
+                    //.background(Color(0xFF3ddc84))
+                )
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.icons8_search_128),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(top = 17.dp)
+                    .requiredSize(50.dp),
+            )
+        }
+
+
+    }
+
+
+}*/
+
+@Composable
+fun CategoryCard(categoryName: Category) {
+    Card(modifier = Modifier
+        .padding(15.dp)
+        .fillMaxWidth()
+        .height(80.dp)
+        .border(
+            // puts a border around the card
+            border = BorderStroke(3.dp, Color(0xFF27187E)),
+            // shapes the card
+            shape = RectangleShape
+        ),
+
+        elevation = CardDefaults.cardElevation(8.dp),
+        // shape = RoundedCornerShape(16.dp)
+    ) {
+
+        Row(verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .background(color = Color(0xFF758BFD), shape = RectangleShape)
+                .fillMaxWidth()
+                .height(80.dp)
+        )
+        {
+
+            Text(
+                //shows the name of the category
+                text = categoryName.category,
+                fontSize = 40.sp,
+                // have it in bold to make it stand out
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterVertically)
+            )
+        }
+    }
+}
 @Composable
 fun ItemCard(item: Item) {
     Card(
@@ -71,28 +174,33 @@ fun ItemCard(item: Item) {
         // puts a shadow under the card to make it pop out
         elevation = CardDefaults.cardElevation(12.dp),
     ) {
-        // Allows for the text to be put on top of the card
-        Box(modifier = Modifier
-            // the size of the box
-            .width(700.dp)
-            .height(200.dp)
-            // changes the color of the box
-            .background(Color(0xFF38A3A5)),
-            //allows for the content to be put into the center of the box
-            contentAlignment = Alignment.Center,
-        ){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                // changes the color of the card
+                .background(color = Color(0xFFAEB8FE), shape = RectangleShape)
+                // changes the size of the card
+                .fillMaxWidth()
+                .height(80.dp)
+        )
+        {
             Text(
-                // takes the text from the category variable
                 text = item.name,
-                // changes the size of the font
-                fontSize = 70.sp,
-                // allows for the font to be in bold
-                fontWeight = FontWeight.Bold,
-                lineHeight = 116.sp,
-                //change the color of the text
+                fontSize = 30.sp,
                 color = Color.Black,
                 modifier = Modifier
-                    .padding(50.dp)
+                    .padding(start = 20.dp)
+                    .align(alignment = Alignment.CenterVertically)
+            )
+
+            Text(
+                text = "$" + item.price + "0",
+                fontSize = 30.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(end = 20.dp)
+                    .align(alignment = Alignment.CenterVertically)
             )
         }
     }
