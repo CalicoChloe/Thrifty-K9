4e0c45348d370dfe62558b7ff6446416caf0d517
Shianne <slesure1@ggc.edu>
2023-11-01 15:58:42 -0400

    Updated build.gradle
    I needed to, so my program will work.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation ("androidx.navigation:navigation-compose:2.7.4")
+    implementation("androidx.navigation:navigation-common-ktx:2.7.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
