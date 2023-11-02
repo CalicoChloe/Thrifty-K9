d59a465ac81a7fb08a4f74e0ad91942c1c0e0b50
Shianne <slesure1@ggc.edu>
2023-11-01 22:23:18 -0400

    Refactored the colors

--- app/src/main/java/com/example/pricingpal/ui/theme/Color.kt
+++ app/src/main/java/com/example/pricingpal/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Pricing pals color palette
+val Persian_indigo = Color(0xFF27187E)
+val Cornflower_blue = Color(0xFF758BFD)
+val Periwinkle = Color(0xFFAEB8FE)
+val Anti_flash_white = Color(0xFFF1F2F6)
