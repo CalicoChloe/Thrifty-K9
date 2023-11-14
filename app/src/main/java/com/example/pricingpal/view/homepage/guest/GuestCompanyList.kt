ad64def7158b5932991e41b354158dd357b992f5
Shianne <slesure1@ggc.edu>
2023-11-14 03:35:12 -0500

    Made Guest Company List UI  with collapse header
    This header is made with a sticky header

--- app/src/main/java/com/example/pricingpal/view/homepage/guest/GuestCompanyList.kt
+++ app/src/main/java/com/example/pricingpal/view/homepage/guest/GuestCompanyList.kt
@@ -1,38 +1,38 @@
 package com.example.pricingpal.view.homepage.guest
 
-import android.provider.ContactsContract.CommonDataKinds.Organization
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DockedSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MediumTopAppBar
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarColors
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -40,6 +40,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -47,79 +48,14 @@ import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pricingpal.R
-import com.example.pricingpal.ui.theme.Anti_flash_white
 import com.example.pricingpal.ui.theme.Cornflower_blue
 import com.example.pricingpal.ui.theme.Periwinkle
 import com.example.pricingpal.ui.theme.Persian_indigo
-import com.example.pricingpal.ui.theme.Pink80
+import com.example.pricingpal.ui.theme.Pink40
 import com.example.pricingpal.ui.theme.Uranian_Blue
-import com.example.pricingpal.view.CategoryCard
-import com.example.pricingpal.view.settingRow
-import io.ktor.http.Url
-
-/*
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Header(){
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
-    Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            MediumTopAppBar(
-                modifier = Modifier
-                    //.fillMaxWidth()
-                    .height(270.dp),
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = Pink80
-                ),
-                title = {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                    ){
-                        Card(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(270.dp),
-                            shape = RectangleShape,
-                            elevation = CardDefaults.cardElevation(12.dp),
-                            colors = CardDefaults.cardColors(containerColor = Cornflower_blue)
-                        ){
-                            Image(
-                                //Imports image from resource folder
-                                painter = painterResource(id = R.drawable.logo),
-                                //description of the image for accessibility
-                                contentDescription = "Pictures of paws",
-                                modifier = Modifier
-                                    .padding(top = 15.dp, start = 25.dp, end = 25.dp, bottom = 5.dp)
-                            )
-                            companySearchBar()
-                        }
-                    }
-                },
-                navigationIcon = {
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                },
-                scrollBehavior = scrollBehavior
-            )
-        },
-    ) {padding ->
-        companyList(paddingValues = padding)
-    }
-}
-
- */
-
-
 
 @Composable
 fun pricingPalBar(){
@@ -130,11 +66,11 @@ fun pricingPalBar(){
     ){
         Card(
             modifier = Modifier
-                .nestedScroll(rememberNestedScrollInteropConnection())
-                .fillMaxWidth()
-                .height(270.dp),
+                //.nestedScroll(rememberNestedScrollInteropConnection())
+                .fillMaxWidth(),
+                //.height(270.dp),
             shape = RectangleShape,
-            elevation = CardDefaults.cardElevation(12.dp),
+            //elevation = CardDefaults.cardElevation(12.dp),
             colors = CardDefaults.cardColors(containerColor = Cornflower_blue)
         ){
             Image(
@@ -145,39 +81,116 @@ fun pricingPalBar(){
                 modifier = Modifier
                     .padding(top = 15.dp, start = 25.dp, end = 25.dp, bottom = 5.dp)
             )
-            companySearchBar()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun companySearchBar(){
-    var loginEmail by remember { mutableStateOf("") }
-    TextField(
+    var searchCompany by remember { mutableStateOf("") }
+    var activeSearch by remember { mutableStateOf(false) }
+
+    Card(
         modifier = Modifier
-            .fillMaxWidth()
-            .height(60.dp)
-            .padding(start = 50.dp, end = 50.dp),
-        value = loginEmail,
-        onValueChange = {loginEmail = it},
-        textStyle = TextStyle.Default.copy(fontSize = 20.sp) ,
-        placeholder = { Text("Search", fontSize = 20.sp) },
-        trailingIcon = {
-            Icon(imageVector = Icons.Filled.Search,
-                contentDescription = "Search Icon",
-                tint = Color.Black,
-                modifier = Modifier
-                    .size(50.dp)
-                    .padding(end = 10.dp)
-            ) },
-        colors = TextFieldDefaults.colors(
-            focusedContainerColor = Uranian_Blue,
-            unfocusedContainerColor = Uranian_Blue,
-            unfocusedIndicatorColor = Uranian_Blue,
-            focusedIndicatorColor = Uranian_Blue
-        ),
+            //.nestedScroll(rememberNestedScrollInteropConnection())
+            //.verticalScroll(rememberScrollState())
+            .fillMaxWidth(),
+            //.shadow(12.dp)
+            //.height(500.dp),
         shape = RectangleShape,
-    )
+        //elevation = CardDefaults.cardElevation(12.dp),
+        colors = CardDefaults.cardColors(containerColor = Cornflower_blue)
+    ){
+        /*
+        Spacer(modifier = Modifier.height(20.dp))
+        DockedSearchBar(
+            modifier = Modifier
+                .fillMaxWidth()
+                //.height(60.dp)
+                .padding(start = 50.dp, end = 50.dp),
+            query = searchCompany,
+            onQueryChange = {searchCompany = it},
+            onSearch = {newSearchCompany -> println("$newSearchCompany")},
+            active = activeSearch,
+            onActiveChange = {activeSearch = it},
+            placeholder = { Text("Search", fontSize = 20.sp) },
+            //windowInsets = SearchBarDefaults.windowInsets,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "Search Icon",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(40.dp)
+                        //.padding(end = 10.dp)
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = "Close Icon",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(40.dp)
+                        //.padding(end = 10.dp)
+                )
+            },
+            colors = SearchBarDefaults.colors(
+                containerColor = Uranian_Blue,
+                dividerColor = Persian_indigo,
+            ),
+            shape = RectangleShape,
+        ) {
+
+        }
+
+         */
+        Spacer(modifier = Modifier.height(20.dp))
+            var searching by remember { mutableStateOf("") }
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp)
+                    .padding(start = 50.dp, end = 50.dp),
+                value = searching,
+                onValueChange = { searching = it },
+                textStyle = TextStyle.Default.copy(fontSize = 20.sp),
+                placeholder = { Text("Search", fontSize = 20.sp) },
+                leadingIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "Search Icon",
+                        tint = Color.Black,
+                        modifier = Modifier
+                            .size(40.dp)
+                            .padding(start = 10.dp)
+                    )
+                }
+                },
+                trailingIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Close Icon",
+                        tint = Color.Black,
+                        modifier = Modifier
+                            .size(40.dp)
+                            .padding(end = 10.dp)
+                    )
+                }
+                },
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Uranian_Blue,
+                    unfocusedContainerColor = Uranian_Blue,
+                    unfocusedIndicatorColor = Uranian_Blue,
+                    focusedIndicatorColor = Uranian_Blue
+                ),
+                shape = RectangleShape,
+            )
+        Spacer(modifier = Modifier.height(20.dp))
+    }
 }
 
 @Composable
@@ -185,6 +198,7 @@ fun companiesTitle(){
     Card(
         shape = RectangleShape,
         modifier = Modifier
+            .padding(top = 15.dp)
             .padding(15.dp),
         elevation = CardDefaults.cardElevation(12.dp),
     ) {
@@ -261,14 +275,23 @@ fun companyName(){
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun companyList(){
-    pricingPalBar()
-    LazyColumn(
-        modifier = Modifier
-            .padding(top = 270.dp)
-    ){
+    LazyColumn {
+        item {
+            pricingPalBar()
+        }
+        stickyHeader {
+            /*
+            Box(modifier = Modifier.fillMaxWidth().background(Color.LightGray)) {
+                Text(text = "Header", fontSize = 24.sp)
+            }
 
+             */
+            companySearchBar()
+            Divider(thickness = 4.dp, color = Persian_indigo)
+        }
         item {
             companiesTitle()
         }
@@ -279,4 +302,5 @@ fun companyList(){
         }
     }
 }
+
 
