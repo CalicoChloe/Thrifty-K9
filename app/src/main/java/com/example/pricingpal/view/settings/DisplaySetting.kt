0f014791be4ec7b4457c3b565d35d2242a3e89bd
Shianne <slesure1@ggc.edu>
2023-11-15 19:22:02 -0500

    Made Display UI

--- app/src/main/java/com/example/pricingpal/view/settings/DisplaySetting.kt
+++ app/src/main/java/com/example/pricingpal/view/settings/DisplaySetting.kt
@@ -1,4 +1,4 @@
-package com.example.pricingpal.view.settings.guestaccount
+package com.example.pricingpal.view.settings
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
@@ -15,35 +15,32 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.pricingpal.R
 import com.example.pricingpal.ui.theme.Cornflower_blue
 import com.example.pricingpal.ui.theme.Periwinkle
 import com.example.pricingpal.ui.theme.Persian_indigo
-import com.example.pricingpal.ui.theme.Uranian_Blue
 import com.example.pricingpal.view.repetitivefunctions.settingNavigationBar
 import com.example.pricingpal.view.repetitivefunctions.settingsBar
-import com.example.pricingpal.view.settings.account.accountBackButton
-import com.example.pricingpal.view.settings.account.accountDialog
-import com.example.pricingpal.view.settings.account.emailEdit
-import com.example.pricingpal.view.settings.account.passwordEdit
 
 @Composable
-fun guestAccountSetting(){
+fun displaySetting(){
     Card(
         modifier = Modifier
             //.padding(start = 40.dp, top = 50.dp, end = 40.dp, bottom = 50.dp)
@@ -65,33 +62,25 @@ fun guestAccountSetting(){
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             settingsBar()
-            Spacer(modifier = Modifier.height(20.dp))
-            guestAccountBackButton()
+
             Spacer(modifier = Modifier.height(30.dp))
-            Text(
-                textAlign = TextAlign.Center,
-                text = "Organization Name ",
-                fontSize = 40.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black,
-            )
+            displayBackButton()
 
-            Spacer(modifier = Modifier.height(20.dp))
+            Spacer(modifier = Modifier.height(50.dp))
+            lightThemeButton()
+            darkThemeButton()
 
-            for(i in 1..4) {
-                guestName()
-            }
             Spacer(modifier = Modifier.height(30.dp))
         }
     }
 }
 
 @Composable
-fun guestAccountBackButton(){
+fun displayBackButton(){
     Card(
         modifier = Modifier
             // padding around the card
-            .padding(10.dp)
+            .padding(5.dp)
             .padding(start = 30.dp, end = 30.dp)
             .border(
                 // puts a border around the card
@@ -122,7 +111,7 @@ fun guestAccountBackButton(){
                     .padding(start = 10.dp, top = 5.dp)
             )
             Text(
-                text = "Guest Accounts",
+                text = "Display",
                 textAlign = TextAlign.Center,
                 fontSize = 40.sp,
                 fontWeight = FontWeight.Bold,
@@ -135,19 +124,21 @@ fun guestAccountBackButton(){
     }
 }
 
+
 @Composable
-fun guestName(){
+fun lightThemeButton(){
     Card(
         modifier = Modifier
             // padding around the card
-            .padding(10.dp)
-            .padding(start = 20.dp, end = 20.dp)
+            .padding(5.dp)
+            .padding(start = 30.dp, end = 30.dp)
             .border(
                 // puts a border around the card
                 border = BorderStroke(4.dp, color = Persian_indigo),
                 // shapes the card
                 shape = RectangleShape
             ),
+        elevation = CardDefaults.cardElevation(8.dp),
         // puts a shadow under the card to make it pop out
     ) {
         Row(
@@ -158,29 +149,72 @@ fun guestName(){
                 .background(color = Cornflower_blue, shape = RectangleShape)
                 // changes the size of the card
                 .fillMaxWidth()
-                .height(110.dp)
+                .height(90.dp)
         )
         {
-            Column() {
-                Text(
-                    text = "Email",
-                    fontSize = 30.sp,
-                    color = Color.Black,
-                    modifier = Modifier
-                        .padding(start = 20.dp)
-                    //.align(alignment = Alignment.CenterVertically)
-                )
+            Text(
+                text = "Light Theme",
+                fontSize = 30.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(start = 20.dp)
+                    .align(alignment = Alignment.CenterVertically)
+            )
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.light_theme),
+                contentDescription = "Light Theme Icon",
+                tint = Color.Black,
+                modifier = Modifier
+                    .size(70.dp)
+                    .padding(end = 20.dp, top = 5.dp)
+            )
+        }
+    }
+}
 
-                Text(
-                    text = "Password",
-                    fontSize = 30.sp,
-                    color = Color.Black,
-                    modifier = Modifier
-                        .padding(start = 20.dp)
-                    //.align(alignment = Alignment.CenterVertically)
-                )
-            }
-            deleteIconDialog()
+@Composable
+fun darkThemeButton(){
+    Card(
+        modifier = Modifier
+            // padding around the card
+            .padding(5.dp)
+            .padding(start = 30.dp, end = 30.dp)
+            .border(
+                // puts a border around the card
+                border = BorderStroke(4.dp, color = Persian_indigo),
+                // shapes the card
+                shape = RectangleShape
+            ),
+        elevation = CardDefaults.cardElevation(8.dp),
+        // puts a shadow under the card to make it pop out
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                // changes the color of the card
+                .background(color = Cornflower_blue, shape = RectangleShape)
+                // changes the size of the card
+                .fillMaxWidth()
+                .height(90.dp)
+        )
+        {
+            Text(
+                text = "Dark Theme",
+                fontSize = 30.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(start = 20.dp)
+                    .align(alignment = Alignment.CenterVertically)
+            )
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.dark_theme1),
+                contentDescription = "Dark Theme Icon",
+                tint = Color.Black,
+                modifier = Modifier
+                    .size(60.dp)
+                    .padding(end = 20.dp, top = 5.dp)
+            )
         }
     }
 }
