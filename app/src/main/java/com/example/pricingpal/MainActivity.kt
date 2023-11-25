291eb72aa103332078f65ae38e22a8cc0ec9e8f6
Chloe Jackson <chloe.jackson@protonmail.com>
2023-11-24 20:47:03 -0500

    Cleaned up MainActivity.kt (removed unneccessary comments)

--- app/src/main/java/com/example/pricingpal/MainActivity.kt
+++ app/src/main/java/com/example/pricingpal/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.pricingpal
 
-
-//import com.example.pricingpal.viewmodel.CategoryViewModel
-//import com.example.pricingpal.model.Database
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import com.example.pricingpal.ui.theme.PricingpalTheme
-import com.example.pricingpal.view.background
 import com.example.pricingpal.viewmodel.NewCategoryViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,23 +13,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            /* Initialize the Category ViewModel
-             * This ViewModel will keep the data loaded properly even when the app status is updated
-             * factory is required to pass arguments to the ViewModel when instantiated (needs MainActivity context)
-             */
-/*            val viewModel = viewModel<CategoryViewModel>(
-                factory = object : ViewModelProvider.Factory {
-                    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                        return CategoryViewModel(context = this@MainActivity) as T
-                    }
-                }
-            )*/
+            //Initialize the viewmodel
             val viewModel: NewCategoryViewModel by viewModels()
-            /** This is what I use to show my UI designs. Loading screen is separated because it uses a different background*/
-            //Loading()
-            //volunteerLoading()
-            //editLoading()
-            background()
+
             PricingpalTheme {
                 PricingPalApp(categories = viewModel.categories)
                 //Log.e("VIEWMODEL", viewModel.categories.toString())
