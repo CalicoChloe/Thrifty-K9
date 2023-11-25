8cce6d10ffa7aa36b7dc5523d839e80fddd5ec70
Chloe Jackson <chloe.jackson@protonmail.com>
2023-11-24 20:47:03 -0500

    Cleaned up MainActivity.kt (removed unneccessary comments)

--- app/src/main/java/com/example/pricingpal/MainActivity.kt
+++ app/src/main/java/com/example/pricingpal/MainActivity.kt
@@ -15,24 +15,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            /* Initialize the Category ViewModel
-             * This ViewModel will keep the data loaded properly even when the app status is updated
-             * factory is required to pass arguments to the ViewModel when instantiated (needs MainActivity context)
-             */
-/*            val viewModel = viewModel<CategoryViewModel>(
-                factory = object : ViewModelProvider.Factory {
-                    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                        return CategoryViewModel(context = this@MainActivity) as T
-                    }
-                }
-            )*/
+            //Initialize the viewmodel
             val viewModel: NewCategoryViewModel by viewModels()
             val window = rememberSize()
-            /** This is what I use to show my UI designs. Loading screen is separated because it uses a different background*/
-            //Loading()
-            //volunteerLoading()
-            //editLoading()
-            //background()
             PricingpalTheme {
                 NonScaffoldNavigateScreens(categories = viewModel.categories, windowSize = window)
                 //Log.e("VIEWMODEL", viewModel.categories.toString())
